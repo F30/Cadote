39939fc4ed398d92e1c8660f1f3e8f5d7ea45cb0
Felix Dreissig <felix.dreissig@fau.de>
2021-04-16 15:00:36 +0000

    Enclavization Pass: Add support for structs and tuples to example and LLVM pass

--- examples/EnclavizationPass/src/main.rs
+++ examples/EnclavizationPass/src/main.rs
@@ -15,6 +15,12 @@ use std::prelude::v1::*;
 extern crate cadote_runtime;
 
 
+#[derive(Debug)]
+struct User {
+  sign_in_count: u64,
+  active: bool
+}
+
 #[no_mangle]
 pub fn foo_enclaved_(param: &i64, lala: &str) -> i64 {
   println!("The number is: {} {}", param, lala);
@@ -25,12 +31,27 @@ fn bar_enclaved_() -> (f64, bool, bool, bool, bool, bool, bool, bool, bool, bool
   (1337.0, true, false, true, false, true, false, true, false, true, false, true)
 }
 
+fn more_enclaved_() -> User {
+  User {
+    sign_in_count: 2,
+    active: true
+  }
+}
+
+fn even_more_enclaved_() -> (i64, i64) {
+  (23, 42)
+}
+
 fn main() {
   let val = 42;
   let lala = "abcdefghi";
   foo_enclaved_(&val, &lala[3..]);
   let x = foo_enclaved_(&23, &lala[4..]);
-  println!("Return value: {}", x);
+  println!("foo return value: {}", x);
   let y = bar_enclaved_();
-  println!("Return value: {:?}", y);
+  println!("bar return value: {:?}", y);
+  let a = more_enclaved_();
+  println!("more return value: {:?}", a);
+  let b = even_more_enclaved_();
+  println!("even more return value: {:?}", b)
 }
